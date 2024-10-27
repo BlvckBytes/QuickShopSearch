cf78b232abb0f9b0c1d2f23b3fb8b6e5d4f2b752
Philip Hell <blvckbytes@gmail.com>
2024-10-27 08:56:33 +0100

    The QuickShopListenerFactory now prints proper plugin-names, as well as the detected version

--- core/src/main/java/me/blvckbytes/quick_shop_search/cache/QuickShopListenerFactory.java
+++ core/src/main/java/me/blvckbytes/quick_shop_search/cache/QuickShopListenerFactory.java
@@ -8,17 +8,21 @@ import java.util.logging.Logger;
 
 public class QuickShopListenerFactory {
 
+  private static final String PLUGIN_NAME = "QuickShop-Hikari";
+
   public static Listener create(Logger logger, QuickShopEventConsumer consumer) {
-    var quickShopReference = Bukkit.getPluginManager().getPlugin("QuickShop-Hikari");
+    var quickShopReference = Bukkit.getPluginManager().getPlugin(PLUGIN_NAME);
 
     if (!(quickShopReference instanceof JavaPlugin quickShopPlugin))
-      throw new IllegalStateException("Could not get a reference to QuickShop-Hikari");
+      throw new IllegalStateException("Could not get a reference to " + PLUGIN_NAME);
 
     var quickShopVersionString = quickShopPlugin.getDescription().getVersion();
     var quickShopVersion = parseVersionString(quickShopVersionString);
 
+    logger.info("Detected " + PLUGIN_NAME + " version " + quickShopVersionString);
+
     if (quickShopVersion.length != 4)
-      throw new IllegalStateException("Expected QuickShop-Hikari's version to be comprised of four parts");
+      throw new IllegalStateException("Expected " + PLUGIN_NAME + "'s version to be comprised of four parts");
 
     if (
       quickShopVersion[0] >= 6 &&
@@ -26,11 +30,11 @@ public class QuickShopListenerFactory {
       quickShopVersion[2] >= 0 &&
       quickShopVersion[3] >  7
     ) {
-      logger.info("Loaded listener-support for > QS 6.2.0.7");
+      logger.info("Loaded listener-support for > " + PLUGIN_NAME + " 6.2.0.7");
       return new QuickShopListener_GT_6207(consumer);
     }
 
-    logger.info("Loaded listener-support for <= QS 6.2.0.7");
+    logger.info("Loaded listener-support for <= " + PLUGIN_NAME + " 6.2.0.7");
     return new QuickShopListener_LTE_6207(consumer);
   }
 
